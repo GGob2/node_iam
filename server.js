6d193b0475ce9e52410328f78a79734dde3cc790
GGOb2 <rkdaudwh13@naver.com>
2023-04-21 19:30:07 +0900

    feat: constructing server

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({
-        success: true,
-    });
+    res.send('자바스크립트로 할지.. Python으로 할 지 고민중..');
 });
 
 app.listen(port, () => {
-    console.log(`server is listening at location: ${process.env.PORT}`);
+    console.log(`server is listening at location: ${port}`);
 });
