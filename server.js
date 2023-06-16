7439c2784bc00740c7f50d09026d836064693858
GGOb2 <rkdaudwh13@naver.com>
2023-06-16 18:03:30 +0900

    feat: basic process

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('자바스크립트로 할지.. Python으로 할 지 고민중..');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.get('/api/:idType', (req, res) => {
+    const idType = req.params.idType;
+    if(idType === 'wallet-id') {
+        res.send('You have selected "Wallet ID\"');
+    } else if(idType === 'personal-id') {
+        res.send('You have selected "Personal ID"'); 
+    } else{
+        res.send("Invalid Type");
+    }
+});
+
+app.post('/requestData', function(req, res) {
+    console.log(req.body);
+    res.send("조회 완료")
 });
 
 app.listen(port, () => {
     console.log(`server is listening at location: ${port}`);
 });
+
